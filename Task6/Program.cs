c31448f809dd5cc27ccf70787cbe4014b98b20fa
Ilya Nurullin <nurullinin@mail.ru>
2017-09-25 00:13:26 +0500

    Add user input

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -8,9 +8,12 @@ namespace Task6
 {
     class Program
     {
-        private static int a1, a2, a3;
+        private static double a1, a2, a3;
         static void Main(string[] args)
         {
+            Console.WriteLine("Ввести а1, а2, а3, N, E. Построить последовательность чисел ак = ак–1 + 2 * ак-2 * ак–3. " +
+                              "Найти первые ее N элементов, такие что | ак – ак–1 | > E. Напечатать последовательность, выделить " +
+                              "искомые элементы и их номера.");
             var defaultColor = Console.ForegroundColor;
             while (true)
             {
@@ -21,15 +24,46 @@ namespace Task6
                 int E = Input.readInt("Введите E");
 
                 int currentN = 0;
-                int i = 5;
 
-                for (int j = 1; j <= 4; j++)
-                    Console.WriteLine(Seq(j));
+                if (Math.Abs(a2 - a1) > E && currentN < N)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("{0} и {1} - {2} и {3}", a1, a2, 1, 2);
+                    Console.ForegroundColor = defaultColor;
+                    currentN++;
+                }
+                else
+                {
+                    Console.WriteLine(a1);
+                    Console.WriteLine(a2);
+                }
 
+                if (Math.Abs(a3 - a2) > E && currentN < N)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("{0} и {1} - {2} и {3}", a2, a3, 2, 3);
+                    Console.ForegroundColor = defaultColor;
+                    currentN++;
+                }
+                else
+                {
+                    Console.WriteLine(a3);
+                }
+
+                int i = 4;
+                bool needAnswer = true;
                 while (currentN < N)
                 {
-                    int a_n = Seq(i);
-                    int a_n_1 = Seq(i - 1);
+                    if (needAnswer && i > 30)
+                    {
+                        Console.Write("Алгоритм расчета рекурсивный, экспоненциальный и продолжение работы алгоритма может занять большое количество времени. Продолжить? [y/n]: ");
+                        var answer = Console.ReadLine();
+                        if (answer != "y")
+                            break;
+                        needAnswer = false;
+                    }
+                    double a_n = Seq(i);
+                    double a_n_1 = Seq(i - 1);
                     if (Math.Abs(a_n - a_n_1) > E)
                     {
                         Console.ForegroundColor = ConsoleColor.Green;
@@ -46,7 +80,7 @@ namespace Task6
             }
         }
 
-        static int Seq(int n)
+        static double Seq(int n)
         {
             switch (n)
             {
